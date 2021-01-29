e61f68489ce87e963594bd1c9e0215d2510b3763
Eric Marques <emarques3@gmail.com>
2021-01-29 17:30:01 -0600

    Adding src folder with main component

--- src/app/components/App/index.tsx
+++ src/app/components/App/index.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { x } from './constants';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Open up App.tsx to start working on your app...{x}</Text>
     </View>
   );
 }
