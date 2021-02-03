bab9b698aa844e567840bab74500b96870c2ae65
Eric Marques <emarques3@gmail.com>
2021-02-03 15:00:21 -0600

    Starting component blinking mechanism

--- src/domains/game/components/Sign/index.tsx
+++ src/domains/game/components/Sign/index.tsx
@@ -1,9 +1,21 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { v4 as uuid } from 'uuid';
 import { Box } from 'domains/game/components/Sign/styles';
 import type { AvailableSigns } from 'domains/game/data/modules/Sign';
+// TODO: check the best import flow
+import { Conductor } from 'domains/game/data/modules/Timing/Conductor';
 
 type SignProps = {
   sign: AvailableSigns;
 };
 
-export const Sign = ({ sign }: SignProps) => <Box sign={sign} />;
+export const Sign = ({ sign }: SignProps) => {
+  useEffect(() => {
+    const id = uuid();
+    Conductor.on(sign.name, id, () => {
+      console.log('COMPONENT BLINKING', sign.name);
+    });
+  }, []);
+
+  return <Box sign={sign} />;
+};
