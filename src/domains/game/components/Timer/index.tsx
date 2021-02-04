26007f8f538c7d06b7fd47bf07fdcf1d37b3505d
Eric Marques <emarques3@gmail.com>
2021-02-03 19:15:17 -0600

    Showing timer after sequence is ready

--- src/domains/game/components/Timer/index.tsx
+++ src/domains/game/components/Timer/index.tsx
@@ -1,9 +1,29 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Text } from 'react-native';
 import { Container } from 'domains/game/components/Timer/styles';
+// TODO: check the best import flow
+import { Conductor } from 'domains/game/data/modules/Timing/Conductor';
+import { getId } from 'helpers/getId';
 
-export const Timer = () => (
-  <Container>
-    <Text style={{ color: 'red' }}>03</Text>
-  </Container>
-);
+export const Timer = () => {
+  const [display, setDisplay] = useState(false);
+
+  useEffect(() => {
+    const id = getId();
+
+    Conductor.on('TIMER', id, () => {
+      console.log('UPDATE STATE');
+      setDisplay(true);
+    });
+
+    return () => {
+      Conductor.off(id);
+    };
+  }, []);
+
+  return (
+    <Container>
+      <Text style={{ color: 'red', display: display ? 'flex' : 'none' }}>03</Text>
+    </Container>
+  );
+};
