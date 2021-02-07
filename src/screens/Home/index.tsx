b147a7104091fc28844adaf70bc6253ed30767d0
Eric Marques <emarques3@gmail.com>
2021-02-06 20:35:31 -0600

    Adding config header

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Image, TouchableOpacity, Text } from 'react-native';
 import type { StackNavigationProp } from '@react-navigation/stack';
-import { MainView, TapToStart } from 'screens/Home/styles';
+import { MainView, TapToStart, HomeContainer, ConfigContainer } from 'screens/Home/styles';
 import type { RootStackParamList } from 'screens/types';
 import { useTranslation } from 'app/translation';
 import { useConfig } from 'domains/config/data/hooks/useConfig';
@@ -17,19 +17,28 @@ export const Home = ({ navigation }: Props) => {
 
   const { currentMode, switchMode } = useConfig();
 
+  const openMenu = () => {};
+
   return (
     <MainView>
-      <TouchableOpacity onPress={() => navigation.navigate('Game')}>
-        <Image
-          resizeMode="contain"
-          source={require('assets/images/logo.png')}
-          style={{ width: 350, height: 120 }}
-        />
-        <TapToStart color="#427FA7">{t('home.tapToStart')}</TapToStart>
-      </TouchableOpacity>
-      <TouchableOpacity onPress={switchMode}>
-        <Text style={{ marginTop: 50 }}>mode: {currentMode}</Text>
-      </TouchableOpacity>
+      <ConfigContainer>
+        <TouchableOpacity onPress={openMenu}>
+          <Text style={{ fontSize: 40 }}>G</Text>
+        </TouchableOpacity>
+      </ConfigContainer>
+      <HomeContainer>
+        <TouchableOpacity onPress={() => navigation.navigate('Game')}>
+          <Image
+            resizeMode="contain"
+            source={require('assets/images/logo.png')}
+            style={{ width: 350, height: 120 }}
+          />
+          <TapToStart color="#427FA7">{t('home.tapToStart')}</TapToStart>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={switchMode}>
+          <Text style={{ marginTop: 50 }}>mode: {currentMode}</Text>
+        </TouchableOpacity>
+      </HomeContainer>
     </MainView>
   );
 };
--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -2,8 +2,13 @@ import styled, { css } from 'styled-components/native';
 
 export const MainView = styled.View`
   flex: 1;
+  justify-content: space-between;
+`;
+
+export const HomeContainer = styled.View`
   align-items: center;
   justify-content: center;
+  flex: 7;
 `;
 
 export const TapToStart = styled.Text<{ color: string }>`
@@ -13,4 +18,11 @@ export const TapToStart = styled.Text<{ color: string }>`
     font-size: 30px;
     margin-top: 30px;
   `}
+`;
+
+export const ConfigContainer = styled.View`
+  flex: 1;
+  justify-content: flex-end;
+  align-items: flex-end;
+  padding-right: 20px;
 `;
