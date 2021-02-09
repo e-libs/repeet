9f6335cc5e47431f3491a67c56568b2daaa63ac9
Eric Marques <emarques3@gmail.com>
2021-02-08 20:08:58 -0600

    Adding game difficulty to config store

--- src/domains/config/data/store/reducer.ts
+++ src/domains/config/data/store/reducer.ts
@@ -1,14 +1,17 @@
 import type { Action } from 'redux-actions';
 import type { ConfigState, ConfigActions } from 'domains/config/data/store/types';
-import { SET_MODE } from 'domains/config/data/store/actionTypes';
+import { SET_DIFFICULTY, SET_MODE } from 'domains/config/data/store/actionTypes';
 import {
   advancedPool,
   advancedPoolSize,
   standardPool,
   standardPoolSize,
 } from 'domains/game/data/modules/Sign';
+import { initialAttempts } from 'domains/game/data/modules/Game/constants';
 
 const initialState: ConfigState = {
+  attempts: initialAttempts,
+  difficulty: 'MEDIUM',
   mode: 'DEFAULT',
   pool: standardPool,
   poolSize: standardPoolSize,
@@ -16,6 +19,20 @@ const initialState: ConfigState = {
 
 export const configReducer = (state = initialState, action: Action<ConfigActions>) => {
   switch (action.type) {
+    case SET_DIFFICULTY: {
+      const { difficulty } = action.payload;
+
+      let attempts = 3;
+
+      if (difficulty === 'EASY') attempts = 5;
+      else if (difficulty === 'HARD') attempts = 2;
+
+      return {
+        ...state,
+        attempts,
+        difficulty,
+      };
+    }
     case SET_MODE: {
       const { mode } = action.payload;
 
