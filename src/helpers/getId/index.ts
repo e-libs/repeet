615206bd27600af04c1079199fe7aebb1b889fac
Eric Marques <emarques3@gmail.com>
2021-02-11 12:28:08 -0600

    Temporarily replacing uuid library by manual generating mechanism, removing dependency

--- src/helpers/getId/index.ts
+++ src/helpers/getId/index.ts
@@ -1,3 +1,15 @@
-import { v4 as uuid } from 'react-native-uuid';
+// Temporarily disabling due to known bug: https://github.com/eugenehp/react-native-uuid/issues/11
+// import { v4 as uuid } from 'react-native-uuid';
+// export const getId = () => uuid();
 
-export const getId = () => uuid();
+export const getId = (length = 30) => {
+  let result = '';
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  const charactersLength = characters.length;
+
+  for (let i = 0; i < length; i += 1) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+
+  return result;
+};
